ac216f5c09640a545f73ffbede54ece6b580ec6f
Jagpreet Bath <jbath@proserveit.com>
2020-09-19 12:56:23 -0400

    Map a Single Point

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,9 +4,10 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-//  Add a marker to the map for Los Angeles, California.
+//  Add a marker to the map
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
+//Add a circle to the map 
 // L.circle([34.0522, -118.2437], {
 //     radius: 100
 //  }).addTo(map);
