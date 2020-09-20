e1fc8aa32fc75b2e6490c9aa9bb21c87502a4c3d
Jagpreet Bath <jbath@proserveit.com>
2020-09-20 12:04:45 -0400

    Updated logic.js file

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   accessToken: API_KEY
@@ -32,11 +32,24 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/JagpreetBath/Mapping_Earthquakes/master/majorAirports.json";
+let torontoData = "https://raw.githubusercontent.com/JagpreetBath/Mapping_Earthquakes/master/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
 // // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  L.geoJson(data, {
+    style: myStyle,
+    onEachFeature: function(feature, layer){
+      layer.bindPopup("<h3> Airline:" + feature.properties.airline + "</h3> <hr><h3>" 
+      + feature.properties.dst + "</h3>");
+    }
+  })
+.addTo(map);
 });
